dfe127eb826e1b7c04ed4bfda9f5662fbe50ba05
oumaima EL HACHIMI <oumaimaelhachimi55@gmail.com>
2024-05-07 15:48:45 +0100

    home page loading

--- src/app/components/board-fs/board-fs.component.ts
+++ src/app/components/board-fs/board-fs.component.ts
@@ -10,15 +10,41 @@ export class BoardFSComponent implements OnInit{
   images=[
     {name: '1.avif', caption:"hello", description:"Galleria requires a value as a collection of images, item template for the higher resolution image and thumbnail template to display as a thumbnail."},
     {name: '2.avif', caption:"by", description:"Galleria can be controlled programmatically using the activeIndex property."},
+    {name: '3.avif', caption:"by", description:"Galleria can be controlled programmatically using the activeIndex property."},
     
 
   ];
-  ngOnInit() {
-      
-  }
   elem=document.documentElement;
   fullscreen(){
     if(this.elem.requestFullscreen){
       this.elem.requestFullscreen();
   }}
+  currentIndex: number = 0;
+  intervalId: any;
+
+  ngOnInit() {
+    // Start automatic sliding
+    this.startInterval();
+  }
+
+  prevSlide() {
+    this.currentIndex = (this.currentIndex === 0) ? (this.images.length - 1) : (this.currentIndex - 1);
+    this.resetInterval();
+  }
+
+  nextSlide() {
+    this.currentIndex = (this.currentIndex === this.images.length - 1) ? 0 : (this.currentIndex + 1);
+    this.resetInterval();
+  }
+
+  startInterval() {
+    this.intervalId = setInterval(() => {
+      this.nextSlide();
+    }, 3000); // Interval set to 3000 milliseconds (3 seconds)
+  }
+
+  resetInterval() {
+    clearInterval(this.intervalId);
+    this.startInterval();
+  }
 }
